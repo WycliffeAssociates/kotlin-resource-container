dbd23ec35d8ebfdeed65aa339c1a98cb0656d588
Joe <jsarabia1247@gmail.com>
2018-09-05 13:45:44 -0400

    ignore unknown

--- src/main/kotlin/org/wycliffeassociates/resourcecontainer/entity/TableOfContents.kt
+++ src/main/kotlin/org/wycliffeassociates/resourcecontainer/entity/TableOfContents.kt
@@ -1,16 +1,18 @@
 package org.wycliffeassociates.resourcecontainer.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
 
 data class TableOfContents(
-        val contents: Content
+    val contents: Content
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Content(
-        val title: String = "",
-        @JsonProperty("sub-title")
-        val subtitle: String = "",
-        val link: String = "",
-        val sections: List<Content> = arrayListOf()
+    var title: String = "",
+    @JsonProperty("sub-title")
+    var subtitle: String = "",
+    var link: String = "",
+    val sections: List<Content> = arrayListOf()
 )
