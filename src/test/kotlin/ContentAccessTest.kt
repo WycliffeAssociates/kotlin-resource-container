d4fbe946c63f81d9b4cec230d7e405a4316002ce
AnonymousWalker <hoanganhtran1998@gmail.com>
2021-02-11 12:19:30 -0500

    refactor test

--- src/test/kotlin/ContentAccessTest.kt
+++ src/test/kotlin/ContentAccessTest.kt
@@ -4,25 +4,27 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
 import java.io.File
 import java.io.FileNotFoundException
-import java.io.IOException
 
 class ContentAccessTest {
     private data class GetContentTestCase(
         val path: String,
         val extension: String,
-        val numberOfStreams: Int
+        val expectedStreams: Int
     )
 
-    private val tempDir = createTempDir()
+    private lateinit var tempDir: File
     private lateinit var testCases: List<GetContentTestCase>
+
 
     @Before
     fun setUp() {
+        tempDir = createTempDir()
         val testCaseResource = javaClass.classLoader.getResourceAsStream("GetRCContentTestCases.json")
         testCaseResource.use {
             testCases = jacksonObjectMapper().readValue(it)
@@ -37,7 +39,7 @@ class ContentAccessTest {
     @Throws(FileNotFoundException::class)
     private fun getRCFile(name: String): File {
         val resource = javaClass.classLoader.getResource(name)
-                ?: throw FileNotFoundException()
+                ?: throw FileNotFoundException("Test resource not found: $name")
         return File(resource.file)
     }
 
@@ -46,13 +48,18 @@ class ContentAccessTest {
     fun getContentFromRC() {
         testCases.forEach { testCase ->
             val rcFile = getRCFile(testCase.path)
-            ResourceContainer.load(rcFile).use {
-                val content = it.getProjectContent(
-                        projectIdentifier = "sng", extension = testCase.extension
+            ResourceContainer.load(rcFile).use { rc ->
+                val content = rc.getProjectContent(
+                    projectIdentifier = "sng", extension = testCase.extension
                 )
 
                 assertNotNull(content)
-                assertEquals(testCase.numberOfStreams, content!!.streams.size)
+                content!!
+                assertEquals(testCase.expectedStreams, content.streams.size)
+                assertFalse(
+                        "Empty file stream found",
+                        content.streams.any { it.value.available() == 0 }
+                )
 
                 content.streams.forEach { entry ->
                     tempDir.resolve(File(entry.key).name).outputStream().use { output ->
@@ -64,21 +71,36 @@ class ContentAccessTest {
     }
 
     @Test
-    fun failOnClosedRC() {
-        val testCase = testCases[0]
+    @Throws(Exception::class)
+    fun failOnClosedZipFile() {
+        val testCase = testCases.first { it.path.endsWith(".zip") }
         val rcFile = getRCFile(testCase.path)
         val rc = ResourceContainer.load(rcFile)
         val content = rc.getProjectContent(extension = testCase.extension)
 
-        rc.close()
-        try {
-            content!!.streams.forEach { entry ->
-                tempDir.resolve(File(entry.key).name).outputStream().use { output ->
-                    output.write(entry.value.read())
-                }
-            }
-        } catch (ex: IOException) {
-            assertEquals("Stream closed", ex.message)
-        }
+        assertNotNull(content)
+        rc.close() // closing the rc (zip) should properly close all opened streams
+
+        assertFalse(
+                "Streams were not properly closed.",
+                content!!.streams.any { it.value.available() != 0 }
+        )
+    }
+
+    @Test
+    fun subsequentAccess() {
+        val testCase = testCases.first { it.path.endsWith(".zip") }
+        val rcFile = getRCFile(testCase.path)
+        val rc = ResourceContainer.load(rcFile)
+        var content = rc.getProjectContent(extension = testCase.extension)
+
+        assertNotNull(content)
+        assertEquals(testCase.expectedStreams, content!!.streams.size)
+
+        // repeat accessing content
+        content = rc.getProjectContent(extension = testCase.extension)
+
+        assertNotNull(content)
+        assertEquals(testCase.expectedStreams, content!!.streams.size)
     }
 }
